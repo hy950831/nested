1edcacf40fc1362602e6b955fc4b061893d2a9aa
Yu Hou <Yu.Hou@data61.csiro.au>
2019-03-18 18:22:10 +1100

    fix stupid cmake stuff

--- helpers.cmake
+++ helpers.cmake
@@ -11,8 +11,8 @@ function(CreateComponent)
     endif()
 
     foreach(item ${CREATE_COMPONENT_C_FILES})
-        list(APPEND pp_cspaces "${CMAKE_CURRENT_BINARY_DIR}/cspace_${item}")
-        list(APPEND pp_cspaces_stripped "cspace_${item}")
+        list(APPEND pp_cspaces ${CMAKE_CURRENT_BINARY_DIR}/cspace_${item})
+        list(APPEND pp_cspaces_stripped cspace_${item})
     endforeach()
 
     foreach(item ${CREATE_COMPONENT_C_FILES})
@@ -20,9 +20,9 @@ function(CreateComponent)
         list(APPEND pp_elfs "${name}")
     endforeach()
 
-    cdl_pp("${CREATE_COMPONENT_MANIFEST}" ${CREATE_COMPONENT_TARGET}_pp_target_test
-        ELF "${pp_elfs}"
-        CFILE "${pp_cspaces}"
+    cdl_pp(${CREATE_COMPONENT_MANIFEST} ${CREATE_COMPONENT_TARGET}_pp_target
+        ELF ${pp_elfs}
+        CFILE ${pp_cspaces}
         )
 
     list(LENGTH pp_elfs len)
@@ -30,34 +30,34 @@ function(CreateComponent)
 
     foreach(i RANGE ${len})
         list(GET pp_elfs ${i} elf)
-        list(GET pp_cspaces ${i} cspace)
+        list(GET pp_cspaces_stripped ${i} cspace)
         list(GET CREATE_COMPONENT_C_FILES ${i} cfile)
 
         set_property(SOURCE ${cspace} PROPERTY GENERATED 1)
         add_executable(${elf} EXCLUDE_FROM_ALL ${cfile} ${cspace})
-        add_dependencies(${elf} ${CREATE_COMPONENT_TARGET}_pp_target_test)
+        add_dependencies(${elf} ${CREATE_COMPONENT_TARGET}_pp_target)
 
-        target_link_libraries("${elf}" sel4tutorials)
+        target_link_libraries(${elf} sel4tutorials)
 
         list(APPEND elf_files "$<TARGET_FILE:${elf}>")
         list(APPEND elf_targets "${elf}")
     endforeach()
 
-    cdl_ld("${CMAKE_CURRENT_BINARY_DIR}/${CREATE_COMPONENT_TARGET}_spec.cdl" ${CREATE_COMPONENT_TARGET}_capdl_spec
+    cdl_ld(${CMAKE_CURRENT_BINARY_DIR}/${CREATE_COMPONENT_TARGET}_spec.cdl ${CREATE_COMPONENT_TARGET}_capdl_spec
         MANIFESTS ${CREATE_COMPONENT_MANIFEST}
         ELF ${elf_files}
         DEPENDS ${elf_targets}
         )
 
-    CapDLToolCFileGen("${CREATE_COMPONENT_TARGET}_cspec" "${CREATE_COMPONENT_TARGET}_cspec.c" "${CMAKE_CURRENT_BINARY_DIR}/${CREATE_COMPONENT_TARGET}_spec.cdl" "${CAPDL_TOOL_BINARY}"
+    CapDLToolCFileGen(${CREATE_COMPONENT_TARGET}_cspec ${CREATE_COMPONENT_TARGET}_cspec.c ${CMAKE_CURRENT_BINARY_DIR}/${CREATE_COMPONENT_TARGET}_spec.cdl "${CAPDL_TOOL_BINARY}"
         MAX_IRQS ${CapDLLoaderMaxIRQs}
         DEPENDS ${${CREATE_COMPONENT_TARGET}_capdl_spec} install_capdl_tool "${CAPDL_TOOL_BINARY}")
 
     BuildCapDLApplication(
-        C_SPEC "${CREATE_COMPONENT_TARGET}_cspec.c"
+        C_SPEC ${CREATE_COMPONENT_TARGET}_cspec.c
         ELF ${elf_files}
-        DEPENDS ${elf_targets} "${CREATE_COMPONENT_TARGET}_cspec"
-        OUTPUT "${CREATE_COMPONENT_TARGET}"
+        DEPENDS ${elf_targets} ${CREATE_COMPONENT_TARGET}_cspec
+        OUTPUT ${CREATE_COMPONENT_TARGET}
         )
 
     SetSeL4Start(${CREATE_COMPONENT_TARGET})
